a1f87c2ab678324eed6059a040a5b64596ed46de
glanyx <mikekornet@live.co.uk>
2020-01-27 11:13:43 +0000

    added help usability

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,3 +1,10 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Pong!");
-}
+  message.channel.send("Pong!");
+};
+
+exports.help = {
+  name: "ping",
+  category: "Fun",
+  description: "Pong!",
+  usage: "ping"
+};
