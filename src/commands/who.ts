7ad9b7fb83302197605efa2e2c4e6cfd85a5faf8
Glanyx <mikekornet@live.co.uk>
2020-11-09 19:50:12 +0000

    fixes for who

--- src/commands/who.ts
+++ src/commands/who.ts
@@ -1,33 +1,34 @@
-import { MessageEmbed, Guild, GuildMember } from "discord.js";
+import { Client, Message, MessageEmbed, Guild, GuildMember } from "discord.js";
 import * as dateFormatLib from "../libs/dateformat-lib";
 
-exports.run = async (client, message, args) => {
-  if (message.mentions.members.first()) {
+exports.run = async (client: Client, message: Message, args: string[]) => {
+
+  const { guild, member } = message
+
+  if (!guild) return
+  if (!member) return
+
+  if (message.mentions.members && message.mentions.members.first()) {
     message.mentions.members.forEach(async member => {
-      const embed = await generateProfile(message.guild, member);
+      const embed = await generateProfile(guild, member);
       await message.channel.send(embed);
     });
     return;
   }
-  const embed = await generateProfile(message.guild, message.member);
+  const embed = await generateProfile(guild, member);
   message.channel.send(embed);
 };
 
-/**
- * 
- * @param {Guild} guild 
- * @param {GuildMember} gMember 
- */
-
-const generateProfile = async (guild, gMember) => {
+const generateProfile = async (guild: Guild, gMember: GuildMember) => {
   let joinPos = 1;
   let roleString = "";
 
   await guild.members.fetch()
   await guild.roles.fetch()
 
-  guild.members.cache.forEach(member => {
-    if (member !== gMember) {
+  guild.members.cache.forEach(async member => {
+    if (member && member !== gMember) {
+      if (!member.joinedAt || !gMember.joinedAt) return
       if (member.joinedAt < gMember.joinedAt) {
         joinPos++;
       }
@@ -43,11 +44,11 @@ const generateProfile = async (guild, gMember) => {
   const highestRole = gMember.roles.highest
 
   return new MessageEmbed()
-    .setAuthor(gMember.user.tag, gMember.user.avatarURL())
-    .setThumbnail(gMember.user.avatarURL())
+    .setAuthor(gMember.user.tag, gMember.user.avatarURL() || undefined)
+    .setThumbnail(gMember.user.avatarURL() || '')
     .setColor(highestRole.color)
     .setTimestamp()
-    .setFooter(`Powered by ${gMember.client.user.username}`)
+    .setFooter(`Powered by ${gMember.client.user?.username || 'LeegionBot'}`)
     .addField("**User**", `${gMember.user.username} [${gMember.user}]`, true)
     .addField("**Status**", gMember.presence.status, true)
     .addField(
