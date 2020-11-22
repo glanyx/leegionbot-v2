828380468989720af93a8f272385f9bc41e68e62
Glanyx <mikekornet@live.co.uk>
2020-11-22 18:34:08 +0000

    added automatic / manual address and text input helper

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,3 +1,5 @@
+import { MessageEmbed, User, Message } from 'discord.js'
+
 export const toTitleCase = (text: string) => {
   return text.replace(
     /\w\S*/g,
@@ -5,4 +7,51 @@ export const toTitleCase = (text: string) => {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     }
   );
+}
+
+interface ITextInput {
+  user: User
+  prompt: string | MessageEmbed
+  timeout?: number
+  limit?: number
+}
+
+export const getTextInput = ({
+  user,
+  prompt,
+  timeout,
+  limit
+}: ITextInput): Promise<string> => {
+
+  const TIMEOUT = timeout || 1800000
+
+  return new Promise<string>((resolve, reject) => {
+
+    user.send(prompt)
+      .then(message => {
+        const collector = message.channel.createMessageCollector(m =>
+          user === m.author,
+          {
+            max: limit,
+            idle: TIMEOUT
+          }
+        )
+
+        collector.on("collect", (m: Message) => {
+          collector.stop()
+          resolve(m.content)
+        })
+
+        collector.on('end', collected => {
+          if (collected.size < 1) {
+            user.send('This message timed out.')
+            reject(new Error('Message timed out.'))
+          }
+        })
+      
+      }).catch(e => {
+        console.log(e)
+        reject(new Error('Message timed out.'))
+      })
+  })
 }
