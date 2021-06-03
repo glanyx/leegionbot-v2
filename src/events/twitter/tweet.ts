8e38ae60d2fca821ce23b1731758ab4752891efc
Glanyx <mikekornet@live.co.uk>
2021-06-03 22:02:54 +0100

    re-added twitter integration

---
+++ src/events/twitter/tweet.ts
@@ -0,0 +1,40 @@
+import { Client, TextChannel, MessageEmbed } from 'discord.js'
+import { TweetEvent, TwitterClient } from '../../utils/twitter'
+import { GuildSetting } from '../../db/models'
+
+interface MultiClient {
+  discordClient: Client
+  twitterClient: TwitterClient
+}
+
+export class Tweet {
+
+  public static async execute(clients: MultiClient, tweet: TweetEvent) {
+
+    const { discordClient } = clients
+    const { data, rule } = tweet
+
+    const guild = discordClient.guilds.cache.get(rule.guildId)
+    if (!guild) return
+
+    GuildSetting.fetchByGuildId(guild.id)
+      .then(setting => {
+        if (!setting) return
+
+        const channel = guild.channels.cache.get(setting.announcementChannelId) as TextChannel
+
+        const embed = new MessageEmbed()
+          .setColor('#1DA1F2')
+          .setTitle(`New Tweet by @${rule.from}`)
+          .setDescription(`${data.text}\n\n[View on Twitter](https://twitter.com/${rule.from}/status/${data.id})`)
+          .setFooter('Via Twitter')
+          .setTimestamp()
+          .setURL(`https://twitter.com/${rule.from}`)
+
+        channel.send(embed)
+
+      })
+
+  }
+
+}
