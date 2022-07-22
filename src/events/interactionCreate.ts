685be47f15b1849dc15710c1b05ad87d749d518e
Mike Kornet <mike.kornet@shl.com>
2022-07-22 13:26:18 +0100

    actual fixes this time?

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,5 +1,6 @@
 import { Client, Interaction } from 'discord.js'
 import { ButtonHandlers } from '../buttonHandlers'
+import { logger } from '../utils'
 
 export class InteractionCreate {
 
@@ -12,6 +13,8 @@ export class InteractionCreate {
 
       const handler = ButtonHandlers.find(item => item.name.toLowerCase() === name.toLowerCase())
       if (handler) {
+        const { guild, user } = interaction
+        logger.info(`Executing Button ${name} | Guild ID ${guild?.id} | User ID ${user.id}`)
         await interaction.deferReply()
         handler.execute({ client, interaction, args: content })
       }
