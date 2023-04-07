6914d93f50ffa510487cb2ed6959d89a38c0973f
Glanyx <mikekornet@live.co.uk>
2023-04-07 19:17:49 +0100

    fix for no roles in ticket embed

--- src/managers/ticketManager.ts
+++ src/managers/ticketManager.ts
@@ -161,7 +161,7 @@ class Ticket {
         value: `${this.member}`,
       }, {
         name: 'Roles',
-        value: `${roles.map(r => `${r}`).join('\n')}`
+        value: roles.length > 0 ? `${roles.map(r => `${r}`).join('\n')}` : '*None*'
       }, {
         name: 'Ticket ID',
         value: `${this.model.guildTicketId}`,
