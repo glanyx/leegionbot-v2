a146edac32abfa1bda66d110970459aaf2d75b8e
Mike Kornet <mike.kornet@shl.com>
2023-09-20 12:52:47 +0100

    initial commit for image recognition

---
+++ src/managers/imageManager.ts
@@ -0,0 +1,69 @@
+import { Client, Colors, EmbedBuilder, Message } from 'discord.js'
+import { ImageTool } from '../utils'
+import { GuildSetting } from '../db/models'
+
+const validContentTypes = [
+  'png',
+  'gif',
+  'jpg',
+  'jpeg',
+]
+
+export class ImageManager {
+
+  private client: Client
+
+  constructor(client: Client) {
+    this.client = client
+    this.init()
+  }
+
+  private init = () => {
+    this.client.on('messageCreate', (message) => {
+      this.checkMessage(message)
+    })
+  }
+
+  private checkMessage = (message: Message) => {
+
+    if (!message.attachments || !message.inGuild()) return
+
+    GuildSetting.fetchByGuildId(message.guildId)
+      .then(async set => {
+
+        if (!set || !set.modLogChannelId) return
+
+        const ch = message.guild.channels.cache.get(set.modLogChannelId) || await message.guild.channels.fetch(set.modLogChannelId)
+        if (!ch || !ch.isTextBased()) return
+
+        message.attachments.forEach(att => {
+          if (validContentTypes.includes(att.contentType || '')) {
+            ImageTool.validateImage(att.url).then(({ report }) => {
+
+              const embed = new EmbedBuilder()
+                .setTitle('AI Art Detected')
+                .setDescription(`AI Art was detected [here](${message.url})`)
+                .setColor(Colors.Red)
+                .setThumbnail(att.url)
+                .setFields([
+                  {
+                    name: 'Human Confidence',
+                    value: `${report.human.confidence}`,
+                    inline: true
+                  }, {
+                    name: 'AI Confidence',
+                    value: `${report.ai.confidence}`,
+                    inline: true
+                  }
+                ])
+
+              ch.send({ embeds: [embed] })
+
+            })
+          }
+        })
+
+      })
+  }
+
+}
---
+++ src/utils/imageTool.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import logger from './logger'
+
+interface ConfidenceModel {
+  confidence: number
+  is_detected: boolean
+}
+
+interface ApiResponse {
+  data: {
+    report: {
+      version: string
+      ai: ConfidenceModel
+      human: ConfidenceModel
+      dall_e: ConfidenceModel
+      midjourney: ConfidenceModel
+      stable_diffusion: ConfidenceModel
+      this_person_does_not_exist: ConfidenceModel
+    }
+  }
+}
+
+export class ImageTool {
+
+  private static url = 'https://v3-atrium-prod-api.optic.xyz/aion/ai-generated/reports'
+
+  public static validateImage = (imageUrl: string) => {
+
+    return axios({
+      method: 'POST',
+      url: this.url,
+      data: {
+        object: imageUrl,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY': process.env.AIORNOT_KEY,
+      }
+    })
+      .then(({ data }: ApiResponse) => data)
+      .catch(e => { throw new Error(e) })
+
+  }
+
+}
