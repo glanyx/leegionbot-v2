c206e8904f7f9b7b35edf9e204e0a2fbfe9eff23
Glanyx <mikekornet@live.co.uk>
2023-11-18 16:00:39 +0000

    check for actual ping

--- src/managers/pingReplyManager.ts
+++ src/managers/pingReplyManager.ts
@@ -15,7 +15,7 @@ export class PingReplyManager {
 
     if (!guild || !member || !reference) return
 
-    if (reference.messageId && message.mentions.repliedUser) {
+    if (reference.messageId && message.mentions.repliedUser && message.mentions.users.map(u => u.id).includes(message.mentions.repliedUser.id)) {
       const target = guild.members.cache.get(message.mentions.repliedUser.id) || await guild.members.fetch(message.mentions.repliedUser.id)
 
       if (target.permissions.has(PermissionFlagsBits.ManageMessages) && !member.permissions.has(PermissionFlagsBits.ManageMessages)) {
