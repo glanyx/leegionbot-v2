cb2bc7fc3b8e21d32a74af913bfa7b4dc613c773
Glanyx <40297607+glanyx@users.noreply.github.com>
2026-05-13 07:57:32 +0200

    Update goLive.ts

--- src/events/twitch/goLive.ts
+++ src/events/twitch/goLive.ts
@@ -24,7 +24,7 @@ export class GoLive {
         .setFooter({ text: `Via Twitch` })
         .setTimestamp()
 
-      item.channel.send({ content: `<@&${item.mentionId}>`, embeds: [embed] })
+      // item.channel.send({ content: `<@&${item.mentionId}>`, embeds: [embed] })
       
     })
 
